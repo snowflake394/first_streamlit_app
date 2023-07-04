004bb949c9d4ee64deabcdcd327e5fef6845ffeb
snowflake394 <138570545+snowflake394@users.noreply.github.com>
2023-07-04 17:47:30 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,7 @@
 import streamlit as st
 
 
-st.title('My parents new healhty diner')
+st.title('My parents new healthy diner')
 st.header('Breakfast Menu')
 st.text('Omega 3 & Blueberry Oatmeal')
 st.text('Kale, Spinach & Rocket Smoothie')
