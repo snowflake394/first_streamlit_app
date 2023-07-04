6c878cea7d30b41271f6352ed1a03d97c38f5a44
snowflake394 <138570545+snowflake394@users.noreply.github.com>
2023-07-04 17:38:14 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,8 @@
 import streamlit as st
 
-st.write("Hello FIRST APP")
+
+st.title('My parents new healhty diner')
+st.header('Breakfast Menu')
+st.text('Omega 3 & Blueberry Oatmeal')
+st.text('Kale, Spinach & Rocket Smoothie')
+st.text('Hard-Boiled Free-Range Egg')
